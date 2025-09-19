882575b8e67633be80359a6eb4355a93113154cd
Georgi Dimov <georgidimov1228@gmail.com>
2025-09-19 19:54:10 +0300

    Set Bulgarian as default language

--- contexts/LanguageContext.tsx
+++ contexts/LanguageContext.tsx
@@ -13,7 +13,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined)
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
-  const [language, setLanguage] = useState<Language>('en')
+  const [language, setLanguage] = useState<Language>('bg')
 
   const t = (enText: string, bgText: string) => {
     return language === 'en' ? enText : bgText
